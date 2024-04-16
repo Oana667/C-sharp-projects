d9272e69efa3a8212ccc2a5f3cc2835264b6c07b
Oana667 <91790771+Oana667@users.noreply.github.com>
2024-04-16 16:05:58 +0300

    Add files via upload

---
+++ SirNr/SirNr/bin/Debug/net6.0/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       
+
+        int suma, produs, ContSir, i, MAritmetica;
+        suma = 0;
+        produs = 1;
+        Console.WriteLine("introuceti numarul elementelor: ");
+        ContSir = int.Parse(Console.ReadLine());
+        float MGeometrica;
+        Console.WriteLine("introuceti elementele: ");
+        for (i=0;i<ContSir;i++)
+        {
+            int nr;
+            nr = int.Parse(Console.ReadLine());
+            suma=suma+nr;
+            produs=produs+nr;
+        }
+        MAritmetica = suma / ContSir;
+        MGeometrica = (float)Math.Pow(produs, (float)1 / ContSir);
+        Console.WriteLine("media aritmetica este: "+ MAritmetica);
+        Console.WriteLine("media geometrica este: " + MGeometrica);
+    }
+}
